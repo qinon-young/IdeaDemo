ab21cca05bd641a77b30baf0bb5f0243eb2b5a7c
qinon <1126799732@qq.com>
2020-09-16 19:46:05 +0800

    修改demo1

--- Demo1/src/com/qinon/test/Demo.java
+++ Demo1/src/com/qinon/test/Demo.java
@@ -3,5 +3,8 @@ package com.qinon.test;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("223");
+        System.out.println("223");
+        System.out.println("223");
+
     }
 }
